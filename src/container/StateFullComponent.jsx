62466c39231bfb6e402d67fdef612bc650cb6b11
root <siskhadhea@gmail.com>
2021-10-17 13:28:24 +0700

    Vidio5

---
+++ src/container/StateFullComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class StateFullComponent extends React.Component{
+    render(){
+      return <p>Statefull component2</p>
+    }
+  }
+
+export default StateFullComponent;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import HelloComponent from './component/HelloComponent';
+import StateFullComponent from './container/StateFullComponent';
 
-const HelloWorld = () => {
-  return <p>Hello World</p>
-}
+//vidio4
+// const HelloWorld = () => {
+//   return <p>Hello World</p>
+// }
 
-ReactDOM.render(<HelloWorld />, document.getElementById('root'));
+//vidio5
+//2cara membuat komponen
+
+//1 stateless component 
+// function HelloComponent(){ //sudah tdk digunakan
+// const HelloComponent = () => { 
+//   return <p>Hello Functional Component</p>
+// }
+
+//2 statefull component
+// class StateFullComponent extends React.Component{
+//   render(){
+//     return <p>Statefull component</p>
+//   }
+// }
+
+
+ReactDOM.render(<HelloComponent />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
